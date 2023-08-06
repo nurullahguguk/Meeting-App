b005b2d518465e8815eca547227ffd25e8af3cb4
Nurullah <Nurullah@NURULLAH>
2023-08-06 16:57:22 +0300

    Add project files.

---
+++ Meeting App/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Meeting_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        //{controller = Home}/{action=index}/id?
+        //loclhost/home => Default
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Meeting App/Program.cs
@@ -0,0 +1,12 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+//MVC saşlonu kurar.
+builder.Services.AddControllersWithViews();
+
+//Projeyi çalıştırmayı sağlar.
+var app = builder.Build();
+
+//Controller/Action/Id?
+app.MapDefaultControllerRoute();
+
+app.Run();
